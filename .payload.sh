c2af2bc7973f20e2810c53b095939bde2ddacdc4
HongJinHyeok <vjinhyeokv@gmail.com>
2022-08-03 18:39:40 +0900

    refactor code

--- .payload.sh
+++ .payload.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/usr/bin/env bash
 yum update -y
 sudo yum install jq -y
 sudo yum install docker -y
@@ -8,9 +8,15 @@ sudo chkconfig docker on
 set -e -x
 bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > /var/log/outline-install.log
 
-echo -e "Management UDP port number :" >> /tmp/outline-install-details.txt
-cat /var/log/outline-install.log | grep "Management port" | cut -d "," -f1 | cut -d " " -f4 >> /tmp/outline-install-details.txt 
-echo -e "VPN TCP / UDP port number :" >> /tmp/outline-install-details.txt
-cat /var/log/outline-install.log | grep "Access key port" | cut -d "," -f1 | cut -d " " -f5 >> /tmp/outline-install-details.txt 
-echo -e "Outline Client Access Key :" >> /tmp/outline-install-details.txt
-sudo docker logs shadowbox | grep "accessUrl" | cut -d " " -f6 | jq '.accessUrl' | head -1 >> /tmp/outline-install-details.txt
+cat > /tmp/outline.json << EOF 
+{ 
+  "ManagementUdpPort" : $(cat /var/log/outline-install.log | grep 'Management port' | cut -d ',' -f1 | cut -d ' ' -f4), 
+  "VpnTcpUdpPort" : $(cat /var/log/outline-install.log | grep 'Access key port' | cut -d ',' -f1 | cut -d ' ' -f5), 
+  "OutlineClientAccessKey" : $(sudo docker logs shadowbox | grep 'accessUrl' | cut -d ' ' -f6 | jq '.accessUrl' | head -1)
+} 
+EOF
+
+
+
+
+
