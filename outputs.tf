edccb1fd67cdef1e8b67f620ea7b80a5a3a6b417
HongJinHyeok <vjinhyeokv@gmail.com>
2022-08-05 02:14:18 +0900

    network bandwidth test

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,9 @@
 
-
 output "ssh_private_key" {
   value     = tls_private_key.tls.private_key_pem
   sensitive = true
 }
 
-output "ssh_ec2_linux_connect" {
-  value = "ssh -v -i ~/.ssh/vpn_ec2_key.pem -o IdentitiesOnly=yes ec2-user@${aws_instance.linux.public_ip}"
-}
-
-output "changed_ipv4_history" {
-  value = "${data.http.myip.body} -> ${aws_instance.linux.public_ip}"
-}
-
-output "sg_id" {
-  value = aws_security_group.vpn_security.id
+output "OutlineClientAccessKey" {
+  value = data.external.access_key.result["accessKey"]
 }
