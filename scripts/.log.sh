2563cc8c9e859bbb940a600681580b543f2964ae
HongJinHyeok <vjinhyeokv@gmail.com>
2022-08-07 02:28:44 +0900

    add logName

--- scripts/.log.sh
+++ scripts/.log.sh
@@ -2,6 +2,7 @@ function log {
   instanceId=$(echo 'aws_instance.linux.id' | terraform console | tr -d '"')
   instanceType=$(echo 'aws_instance.linux.instance_type' | terraform console | tr -d '"')
   instanceAZ=$(echo 'aws_instance.linux.availability_zone' | terraform console | tr -d '"')
+  logName=$LOGNAME
 
   if [ -z "$1" ]
   then
@@ -17,7 +18,8 @@ function log {
   --arg instanceAZ "$instanceAZ" \
   --arg instanceState "$instanceState" \
   --arg timestamp "$timestamp" \
-  '.instanceId=$instanceId|.instanceType=$instanceType|.instanceAZ=$instanceAZ|.instanceState=$instanceState|.timestamp=$timestamp' >> ./.history.log
+  --arg logName "$logName" \
+  '.instanceId=$instanceId|.instanceType=$instanceType|.instanceAZ=$instanceAZ|.instanceState=$instanceState|.timestamp=$timestamp|.logName=$logName' >> ./.history.log
 }
 
 log $1
