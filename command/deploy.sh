7cba7c8883b2ec5375bd6c809799fd2b2bcb6621
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-10 21:38:09 +0900

    add function create_default_tfvars()

--- command/deploy.sh
+++ command/deploy.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash 
+set -e -o pipefail
 
-terraform apply --auto-approve -lock=false -var-file="terraform.tfvars" && bash ./scripts/.log.sh 
+craete_default_tfvars() {
+    if [ ! -f ./terraform.tfvars.json ]; then
+        echo "Create terraform.tfvars.json ✅"
+cat <<EOF > "./terraform.tfvars.json"
+{
+    "aws_region": "us-east-1",
+    "ec2_ami": "ami-0cff7528ff583bf9a",
+    "instance_type": "c4.large"
+}
+EOF
+    fi
+}
+
+command() {
+    terraform apply --auto-approve -lock=false -var-file="terraform.tfvars.json" && bash ./scripts/.log.sh 
+}
+
+main() {
+    craete_default_tfvars && command 
+}
+main
+
