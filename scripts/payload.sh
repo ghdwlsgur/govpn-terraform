76d43e4c945f8fa6a4c7f8b8654cdfa5401283ba
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-23 13:53:46 +0900

    ♻️ change cat to <

--- scripts/payload.sh
+++ scripts/payload.sh
@@ -10,8 +10,8 @@ bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-serve
 
 cat > /tmp/outline.json << EOF 
 { 
-  "ManagementUdpPort" : $(cat /var/log/outline-install.log | grep 'Management port' | cut -d ',' -f1 | cut -d ' ' -f4), 
-  "VpnTcpUdpPort" : $(cat /var/log/outline-install.log | grep 'Access key port' | cut -d ',' -f1 | cut -d ' ' -f5), 
+  "ManagementUdpPort" : $(< /var/log/outline-install.log grep "Management port" | cut -d ',' -f1 | cut -d ' ' -f4), 
+  "VpnTcpUdpPort" : $(< /var/log/outline-install.log grep 'Access key port' | cut -d ',' -f1 | cut -d ' ' -f5), 
   "OutlineClientAccessKey" : $(sudo docker logs shadowbox | grep 'accessUrl' | cut -d ' ' -f6 | jq '.accessUrl' | head -1)
 } 
 EOF
