b8cb7bbccf9c5f9e64f9e3823223127364199e2f
HongJinHyeok <vjinhyeokv@gmail.com>
2022-12-17 21:52:34 +0900

    > to |

--- module/instance/access_key.sh
+++ module/instance/access_key.sh
@@ -5,6 +5,6 @@ region="$1"
 path=$(echo ../../terraform.tfstate.d/"$region")
 
 apiUrl=$(jq ".ApiUrl" "$path"/outline.json | sed 's/\"//g')
-accessKey=$(curl --insecure -X POST "$apiUrl"/access-keys) > jq .accessUrl | sed 's/\"//g'
+accessKey=$(curl --insecure -X POST "$apiUrl"/access-keys) | jq .accessUrl | sed 's/\"//g'
 
 jq -n --arg accessKey "$accessKey" '{"accessKey": $accessKey}' 
