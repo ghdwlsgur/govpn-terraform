55d7931204e9b72d7b3389eb42b3a5ca7fffbcf1
HongJinHyeok <vjinhyeokv@gmail.com>
2022-12-17 20:30:09 +0900

    update to apiurl (not accessKey)

--- scripts/payload.sh
+++ scripts/payload.sh
@@ -6,17 +6,15 @@ sudo service docker start
 sudo chkconfig docker on
 
 set -e -x
-bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > /var/log/outline-install.log
+sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > /var/log/outline-install.log
 
 cat > /tmp/outline.json << EOF 
 { 
   "ManagementUdpPort" : $(< /var/log/outline-install.log grep "Management port" | cut -d ',' -f1 | cut -d ' ' -f4), 
   "VpnTcpUdpPort" : $(< /var/log/outline-install.log grep 'Access key port' | cut -d ',' -f1 | cut -d ' ' -f5), 
-  "OutlineClientAccessKey" : $(sudo docker logs shadowbox | grep 'accessUrl' | cut -d ' ' -f6 | jq '.accessUrl' | head -1)
+  "ApiUrl" : $(< /var/log/outline-install.log grep 'apiUrl' | jq '.apiUrl' | sed 's/\"//g')
 } 
 EOF
-
-
 
 
 
