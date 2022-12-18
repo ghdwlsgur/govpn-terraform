fefd3c9ed893700067239ea941074f71a0a81ad1
HongJinHyeok <vjinhyeokv@gmail.com>
2022-12-18 17:24:38 +0900

    troubleshoot

--- module/instance/access_key.sh
+++ module/instance/access_key.sh
@@ -6,7 +6,7 @@ path=$(echo ../../terraform.tfstate.d/"$region")
 apiUrl=$(jq ".ApiUrl" "$path"/outline.json | sed 's/\"//g')
 
 
-accessKey=$(curl --insecure -sX POST https://43.201.31.119:18823/wi42o8gCZ-nSelFl22IuNw/access-keys | jq '.accessUrl' | sed 's/\"//g')
+accessKey=$(curl --insecure -sX POST "$apiUrl"/access-keys | jq '.accessUrl' | sed 's/\"//g')
 jq -n --arg accessKey "$accessKey" '{"accessKey": $accessKey}' 
 
 
