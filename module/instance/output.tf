4f74a69eedb4448f8c1e7f0cbc4fb68c694eae23
HongJinHyeok <redmax45@naver.com>
2023-04-19 10:16:21 +0900

    add comment (when using terraform console)

--- module/instance/output.tf
+++ module/instance/output.tf
@@ -1,13 +1,16 @@
 
 
+// module.instance.OutlineClientAccessKey
 output "OutlineClientAccessKey" {
   value = data.external.access_key.result["accessKey"]
 }
 
+// module.instance.Region
 output "Region" {
   value = var.aws_region
 }
 
+// module.instance.SecurityGroupID
 output "SecurityGroupID" {
   value = aws_security_group.govpn_security.id
 }
